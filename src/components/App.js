37090d37aea36008f3b23ac5fa137413d4f265be
Ananya <gananya320@gmail.com>
2022-07-26 18:46:34 +0530

    Added code for ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,31 @@
 import React from "react";
+import {data} from '../data';
+import  Navbar from './Navbar';
+import MovieCard from "./MovieCard";
+
 function App() {
   return (
-    <div className="App">
-      Project Setup Done
+    <div className="App" style={styles.App}>
+    <Navbar/>
+    <div className="main">
+      <div className="tabs">
+      <div className="tab">Movies</div>
+      <div className="tab">Favourites</div>
+      </div>
+      <div className="list">   {/*This is a list each movie cart will come inside this. */}
+    {data.map(movie=>(   
+             <MovieCard movie={movie}/>
+      ))}        {/* Each data inside data file is considered as a movie and it will be an object  */}
+      </div>
+    </div>
     </div>
   );
+}
+
+const styles={
+  App:{
+   backgroundColor:"#a71920"
+  }
 }
 
 export default App;
---
+++ src/components/MovieCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class MovieCard extends React.Component{
+ 
+    render(){
+        const {movie}=this.props;
+        return(
+            <div className="movie-card">
+               <div className="left">
+                <img alt="movie-poster" src={movie.Poster}/>
+               </div>
+               <div className="right">
+                <div className="title">{movie.Title}</div>
+                {/* <div>Description:</div> */}
+                <div className="title">{movie.Plot}</div>
+
+               </div>
+                </div>
+        )
+    }
+}
+
+export default MovieCard;
